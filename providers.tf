9bb484be4ea549cc9a7ce0f1630fd74547bbcf11
armzig <bellotaophyc@outlook.com>
2023-07-18 20:02:03 +0100

    actioned the providers.tf file

--- providers.tf
+++ providers.tf
@@ -12,10 +12,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id = "61230797-2385-4403-8a23-553c4a8da920"
-  client_id       = "bd330f5d-22ed-487c-b29d-5d8e88eff76f"
-  tenant_id       = "cfd11325-8f42-48d9-8d1a-dd495c0916ae"
-  client_secret   = "VDa8Q~asta-4OBc~kzb-sWxHjL0rnC4hh~wV2b-2"
     
 }
